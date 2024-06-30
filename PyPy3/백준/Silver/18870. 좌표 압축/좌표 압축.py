e47be731de332517e24eef8af071ead7b614b302
황태균 <htg1616@gmail.com>
2024-06-30 15:19:47 +0900

    [Silver II] Title: 좌표 압축, Time: 1096 ms, Memory: 282104 KB -BaekjoonHub

--- PyPy3/백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ PyPy3/백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -2,14 +2,7 @@ import sys
 input = sys.stdin.readline
 n = int(input())
 nums = list(map(int, input().split()))
-x = [[nums[i], i] for i in range(n)]
-x.sort()
-temp = x[0][0]
-x[0][0] = 0
-for i in range(1, n):
-    if x[i][0] == temp:
-        x[i][0] = x[i-1][0]
-    else:
-        x[i][0], temp = x[i-1][0]+1, x[i][0]
-x.sort(key=lambda x: x[1])
-print(*[i[0] for i in x])
+unique_nums = sorted(set(nums))
+idx_dict = {unique_nums[i]:i for i in range(len(unique_nums))}
+for i in nums:
+    print(idx_dict[i], end=' ')
