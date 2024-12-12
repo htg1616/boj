4816eeb05425358304424e88aa9cadb039fba12f
황태균 <htg1616@gmail.com>
2024-12-12 09:17:37 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define X first
+#define Y second
+typedef long long ll;
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while(t--){
+        int a, b;
+        cin >> a >> b;
+        cout << "yes" << endl;
+    }
+    return 0;
+}
